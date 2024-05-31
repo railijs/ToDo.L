20bc6cf39c28ea4babbacdd37ff98adb0e624bee
railijs <126756192+railijs@users.noreply.github.com>
2024-05-31 10:07:01 +0300

    Delete app directory

--- app/views/404.view.php
+++
@@ -1,9 +0,0 @@
-<?php require "../app/views/components/header.php" ?>
-
-<div class="min-h-screen flex flex-col items-center justify-center bg-red-700">
-  <h1 class="text-9xl font-extrabold text-white tracking-widest">404</h1>
-  <p class="text-2xl font-semibold mt-2 text-white">Lapa nepastāv</p>
-  <a href="/" class="mt-6 px-8 py-4 bg-white text-purple-800 font-semibold rounded shadow-md hover:bg-gray-100">Go Home</a>
-</div>
-
-<?php require "../app/views/components/footer.php"; ?>
